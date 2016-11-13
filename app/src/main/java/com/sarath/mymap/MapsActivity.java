f7d0931b53bdf0a5e641a13d3d7733b60b96babb
creativecoder567 <sarathcreativethinker@gmail.com>
2016-11-13 17:32:55 +0530

    Initial commit

---
+++ app/src/main/java/com/sarath/mymap/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.sarath.mymap;
+
+import android.graphics.Color;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        BitmapDescriptor icon = BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker);
+        // Add a marker in Sydney and move the camera
+        LatLng India = new LatLng(12.976599, 77.602047);
+        mMap.addMarker(new MarkerOptions().position(India).title("Marker in Bangalore").icon(icon)).showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(India));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(India,12.0f));
+
+        Circle circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(12.976599, 77.602047))
+                .radius(1000)
+                .strokeColor(Color.RED)
+                .fillColor(Color.BLUE));
+    }
+}
